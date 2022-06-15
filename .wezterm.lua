608bfbd90a370d68ace2f5570150cf977ae26e3b
Stef Dunlap <stef@kindrobot.ca>
2022-06-15 19:23:25 -0400

    add .wezterm.lua

---
+++ .wezterm.lua
@@ -0,0 +1,31 @@
+return {
+  ssh_domains = {
+    {
+      name = "tilde.town",
+      remote_address = "tilde.town",
+      username = "kindrobot",
+      remote_wezterm_path = "/home/kindrobot/bin/wezterm"
+    },
+    {
+      name = "tilde.team",
+      remote_address = "tilde.team",
+      username = "kindrobot",
+      remote_wezterm_path = "/home/kindrobot/bin/wezterm"
+    },
+    {
+      name = "tacotuesday",
+      remote_address = "192.168.1.100",
+      username = "kindrobot",
+    },
+    {
+      name = "notjustakeyboard",
+      remote_address = "notjustakeyboard.local",
+      username = "kindrobot",
+    },
+    {
+      name = "skylab.org",
+      remote_address = "skylab.org",
+      username = "kindrobot",
+    }
+  }
+}
