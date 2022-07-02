2165f205e0e76cf8c45b12a4c9e49631b819871d
Stef Dunlap <stef@kindrobot.ca>
2022-07-02 14:00:01 -0400

    Add spacemacs and email config

---
+++ .config/mu4e/accounts/gmail.el
@@ -0,0 +1,17 @@
+(add-to-list 'mu4e-contexts
+(make-mu4e-context
+  :name "gmail"
+  :match-func (lambda (msg)
+                (when msg
+                  (mu4e-message-contact-field-matches msg
+                     :to "apocryphalauthor@gmail.com")))
+  :vars '((user-mail-address                . "apocryphalauthor@gmail.com")
+          (message-sendmail-extra-arguments . ("-a" "gmail"))
+          (mu4e-sent-messages-behavior      . delete)
+          (mu4e-drafts-folder               . "/gmail/Drafts")
+          (mu4e-trash-folder                . "/gmail/Trash")
+          (mu4e-sent-folder                 . "/gmail/Sent")
+          (mu4e-maildir-shortcuts           . (("/gmail/INBOX" . ?i)
+                                               ("/gmail/Drafts"         . ?d)
+                                               ("/gmail/Sent" . ?s)))
+          (user-full-name                   . "Stef Dunlap"))))
---
+++ .config/mu4e/accounts/team.el
@@ -0,0 +1,17 @@
+(add-to-list 'mu4e-contexts
+(make-mu4e-context
+  :name "team"
+  :match-func (lambda (msg)
+                (when msg
+                  (mu4e-message-contact-field-matches msg
+                     :to "kindrobot@tilde.team")))
+  :vars '((user-mail-address                . "kindrobot@tilde.team")
+          (message-sendmail-extra-arguments . ("-a" "team"))
+          (mu4e-sent-messages-behavior      . sent)
+          (mu4e-drafts-folder               . "/team/Drafts")
+          (mu4e-trash-folder                . "/team/Trash")
+          (mu4e-sent-folder                 . "/team/Sent")
+          (mu4e-maildir-shortcuts           . (("/team/INBOX" . ?i)
+                                               ("/team/Drafts"         . ?d)
+                                               ("/team/Sent" . ?s)))
+          (user-full-name                   . "Stef Dunlap"))))
