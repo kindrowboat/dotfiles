7aa7e0744e32f185cf3f79033a19c9ec9231dd29
Stef Dunlap <stef@kindrobot.ca>
2022-07-02 23:36:56 -0400

    add hello address

---
+++ .config/mu4e/accounts/hello.el
@@ -0,0 +1,17 @@
+(add-to-list 'mu4e-contexts
+(make-mu4e-context
+  :name "hello"
+  :match-func (lambda (msg)
+                (when msg
+                  (mu4e-message-contact-field-matches msg
+                     :to "hello@kindrobot.ca")))
+  :vars '((user-mail-address                . "hello@kindrobot.ca")
+          (message-sendmail-extra-arguments . ("-a" "hello"))
+          (mu4e-sent-messages-behavior      . sent)
+          (mu4e-drafts-folder               . "/hello/Drafts")
+          (mu4e-trash-folder                . "/hello/Trash")
+          (mu4e-sent-folder                 . "/hello/Sent")
+          (mu4e-maildir-shortcuts           . (("/hello/INBOX" . ?i)
+                                               ("/hello/Drafts"         . ?d)
+                                               ("/hello/Sent" . ?s)))
+          (user-full-name                   . "kindrobot"))))
